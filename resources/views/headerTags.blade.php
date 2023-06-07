21b67357ef2623a9dc20a31263aeec88a8c3df43
kteja1989 <krishnatejamusti@gmail.com>
2023-06-07 16:04:45 +0530

    Made changes to the header tags, created a separate blade file

---
+++ resources/views/headerTags.blade.php
@@ -0,0 +1,38 @@
+<!-- meta tag -->
+        <meta charset="utf-8">
+        <title>Meissa Software Solutions Private Limited</title>
+        <meta name="description" content="">
+        <!-- responsive tag -->
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- favicon -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
+        <!-- Bootstrap v4.4.1 css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
+        <!-- font-awesome css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/font-awesome.min.css') }}">
+        <!-- flaticon css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/fonts/flaticon.css') }}">
+        <!-- animate css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/animate.css') }}">
+        <!-- owl.carousel css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/owl.carousel.css') }}">
+        <!-- slick css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/slick.css') }}">
+        <!-- off canvas css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/off-canvas.css') }}">
+        <!-- magnific popup css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/magnific-popup.css') }}">
+        <!-- Main Menu css -->
+        <link rel="stylesheet" href="{{ asset('/css/rsmenu-main.css') }}">
+        <!-- spacing css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/rs-spacing.css') }}">
+        <!-- style css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}"> <!-- This stylesheet dynamically changed from style.less -->
+        <!-- responsive css -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/responsive.css') }}">
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
